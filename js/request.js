c20983c3e2599dffb9b241ffb7cf9859930f445c
bant <kumir@ya.ru>
2022-02-08 20:16:23 +0300

    =

--- js/request.js
+++ js/request.js
@@ -1,21 +1,21 @@
 $( document ).ready(function() {	
-  $('#request').submit(function(){	  
-	$(".wait").css('display', 'flex');
-    var from=$(this).children('#startDate').val();
-    var to=$(this).children('#endDate').val();    
-    $.ajax({
-      type: "POST",
-      dataType: "text",
-      url: "php/handler.php",
-      data: {
-        from : from,
-        to : to
-      },
-      success: function(msg){
-        $("#result").html(msg);
-		$(".wait").css('display', 'none');
-      }
-    });
-    return false;
-  });  
+	$('#request').submit(function(){	  
+		$(".wait").css('display', 'flex');
+		var from=$(this).children('#startDate').val();
+		var to=$(this).children('#endDate').val();    
+		$.ajax({
+			type: "POST",
+			dataType: "text",
+			url: "php/handler.php",
+			data: {
+				from : from,
+				to : to
+			},
+			success: function(msg){
+				$("#result").html(msg);
+				$(".wait").css('display', 'none');
+			}
+		});
+		return false;
+	});  
 });
